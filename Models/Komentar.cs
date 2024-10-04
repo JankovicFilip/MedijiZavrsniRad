e78fd6c550873d4b822474108bc7fcba34999c29
Filip Jankovic <filip.vno.jankovic@gmail.com>
2024-10-04 16:20:00 +0200

    test 3

--- Models/Komentar.cs
+++ Models/Komentar.cs
@@ -5,13 +5,15 @@ namespace MedijiZavrsniRad.Models
     public class Komentar : Entitet
     {
         public string? Opis { get; set; }
-        //public int MedijId { get; set; }
+        //[ForeignKey("Medij")]
+        public int? Medij { get; set; }
 
         [ForeignKey("Medij")]
-        public Medij? Medij { get; set; }
-        //public int KorisnikId { get; set; }
-            
-        [ForeignKey("Korisnici")]
-        public Korisnik? Korisnik { get; set; }
+        public Medij? Mediji { get; set; }
+
+        //[ForeignKey("Korisnik")]
+        public int? Korisnik { get; set; }
+        [ForeignKey("Korisnik")]
+        public Korisnik? Korisnici { get; set; }
     }
 }
