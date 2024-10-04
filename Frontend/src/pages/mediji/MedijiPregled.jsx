d58e6649dae897105c677f638b7cbca323621fe9
Filip Jankovic <filip.vno.jankovic@gmail.com>
2024-10-04 19:17:02 +0200

    6

--- Frontend/src/pages/mediji/MedijiPregled.jsx
+++ Frontend/src/pages/mediji/MedijiPregled.jsx
@@ -1,11 +1,21 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import MedijiService from "../../services/MedijService"
+import { Table } from "react-bootstrap";
 
 
 export default function MedijiPregled() {
 
+    const[mediji, setMediji] = useState();
+
     async function dohvatiMedije(){
-        await MedijiService.get
+        const odgovor = await MedijiService.get();
+
+        if(odgovor.greska){
+            alert(odgovor.poruka)
+            return
+        }
+        setMediji(odgovor.poruka)
+
     }
 
     useEffect(()=>{
@@ -15,7 +25,36 @@ export default function MedijiPregled() {
 
     return (
         <>
-        ovdje ce doci pregled medija
+            <Table striped bordered hover responsive>
+                <thead>
+                    <tr>
+                        <th>Naziv</th>
+                        <th>Opis</th>
+                        <th>Vrsta</th>
+                        <th>Genre</th>
+                        <th>Akcija</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {mediji && mediji.map((medij,index)=>(
+                        <tr key={index}>
+                            <td>
+                                {medij.naziv}
+                            </td>
+                            <td>
+                                {medij.opis}
+                            </td>
+                            <td>
+                                {medij.vrsta}
+                            </td>
+                            <td>
+                                {medij.genre}
+                            </td>
+                            <td>akcija</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
         </>
     )
 }
--- Frontend/src/services/MedijService.js
+++ Frontend/src/services/MedijService.js
@@ -6,8 +6,12 @@ async function get(){
     return await HttpService.get('/Medij')
     .then((odgovor)=>{
         console.log(odgovor.data)
+        return{greska: false, poruka: odgovor.data}
     })
-    .catch((e)=>console.log(e))
+    .catch((e)=>{
+        console.log(e)
+        return {greska: true, poruka: "problem kod dohvacanja medija"}
+    })
 }
 
 export default {
