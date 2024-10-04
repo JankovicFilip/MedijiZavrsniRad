32074c3f14b3da7cedc1df103cf7e219049093e0
Filip Jankovic <filip.vno.jankovic@gmail.com>
2024-10-04 13:30:14 +0200

    1

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,22 +6,7 @@ function App() {
 
   return (
     <>
-      <div>
-        
-        
-      </div>
-      <h1>Mediji</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      Ovdje dođe moja app
     </>
   )
 }
