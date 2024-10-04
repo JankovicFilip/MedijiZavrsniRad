666143ad3a3e2dfc628e8d7c7ac1997967a2b77c
Filip Jankovic <filip.vno.jankovic@gmail.com>
2024-10-04 15:20:09 +0200

    Pokretanje komentar controllera

---
+++ Controllers/KomentarController.cs
@@ -0,0 +1,30 @@
+﻿using MedijiZavrsniRad.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedijiZavrsniRad.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class KomentarController : ControllerBase
+    {
+        private readonly MedijiContext _context;
+        public KomentarController(MedijiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_context.Komentari);
+        }
+
+        [HttpGet]
+        [Route("{sifra:int}")]
+        public IActionResult GetBySifra(int sifra)
+        {
+            return Ok(_context.Komentari.Find(sifra));
+        }
+
+    }
+}
