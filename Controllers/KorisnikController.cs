10984cae0ded921a60455ee92980079ba7f9b535
Filip Jankovic <filip.vno.jankovic@gmail.com>
2024-10-17 18:01:47 +0200

    Korisnik Controller uz DTO, slijedi početak Korisnik frontenda

--- Controllers/KorisnikController.cs
+++ Controllers/KorisnikController.cs
@@ -1,5 +1,7 @@
-﻿using MedijiZavrsniRad.Data;
+﻿using AutoMapper;
+using MedijiZavrsniRad.Data;
 using MedijiZavrsniRad.Models;
+using MedijiZavrsniRad.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,51 +9,107 @@ namespace MedijiZavrsniRad.Controllers
 {
     [ApiController]
     [Route("api/v1/[controller]")]
-    public class KorisnikController : ControllerBase
+    public class KorisnikController(MedijiContext context, IMapper mapper) : MedijiZavrsniRadController(context, mapper)
     {
-        private readonly MedijiContext _context;
-        public KorisnikController(MedijiContext context)
-        {
-            _context = context;
-        }
+        
         [HttpGet]
-        public IActionResult Get()
+        public ActionResult<List<KorisnikDTORead>> Get()
         {
-            return Ok(_context.Korisnici);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new { poruka = ModelState });
+            }
+            try
+            {
+                return Ok(_mapper.Map<List<KorisnikDTORead>>(_context.Korisnici));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { poruka = ex.Message });
+            }
         }
         [HttpGet]
         [Route("{sifra:int}")]
-        public IActionResult GetBySifra(int sifra)
+        public ActionResult<KorisnikDTORead> GetBySifra(int sifra)
         {
-            return Ok(_context.Korisnici.Find(sifra));
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new { poruka = ModelState });
+            }
+            Korisnik? e;
+            try
+            {
+                e = _context.Korisnici.Find(sifra);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { poruka = ex.Message });
+            }
+            if (e == null)
+            {
+                return NotFound(new { poruka = "Korisnik ne postoji u bazi" });
+            }
+
+            return Ok(_mapper.Map<KorisnikDTORead>(e));
         }
 
         [HttpPost]
-        public IActionResult Post(Korisnik korisnik)
+        public IActionResult Post(KorisnikDTOInsertUpdate dto)
         {
-            _context.Korisnici.Add(korisnik);
-            _context.SaveChanges();
-            return StatusCode(StatusCodes.Status201Created, korisnik);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new { poruka = ModelState });
+            }
+            try
+            {
+                var e = _mapper.Map<Korisnik>(dto);
+                _context.Korisnici.Add(e);
+                _context.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created, _mapper.Map<KorisnikDTORead>(e));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { poruka = ex.Message });
+            }
 
         }
         [HttpPut]
         [Route("{sifra:int}")]
         [Produces("application/json")]
-        public IActionResult Put(int sifra, Korisnik korisnik)
+        public IActionResult Put(int sifra, KorisnikDTOInsertUpdate dto)
         {
 
-            var smjerBaza = _context.Korisnici.Find(sifra);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new { poruka = ModelState });
+            }
+            try
+            {
+                Korisnik? e;
+                try
+                {
+                    e = _context.Korisnici.Find(sifra);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(new { poruka = ex.Message });
+                }
+                if (e == null)
+                {
+                    return NotFound(new { poruka = "Korisnik ne postoji u bazi" });
+                }
 
-            // za sada rucno, kasnije mapper
-            smjerBaza.Ime = korisnik.Ime;
-            smjerBaza.Prezime = korisnik.Prezime;
-            smjerBaza.Email = korisnik.Email;
-            smjerBaza.Password = korisnik.Password;
+                e = _mapper.Map(dto, e);
 
-            _context.Korisnici.Update(smjerBaza);
-            _context.SaveChanges();
+                _context.Korisnici.Update(e);
+                _context.SaveChanges();
 
-            return Ok(new { poruka = "Uspjesno promijenjeno" });
+                return Ok(new { poruka = "Uspješno promjenjeno" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { poruka = ex.Message });
+            }
 
 
         }
@@ -61,13 +119,33 @@ namespace MedijiZavrsniRad.Controllers
         public IActionResult Delete(int sifra)
         {
 
-            var smjerBaza = _context.Korisnici.Find(sifra);
-
-
-            _context.Korisnici.Remove(smjerBaza);
-            _context.SaveChanges();
-
-            return Ok(new { poruka = "Uspjesno obrisano" });
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new { poruka = ModelState });
+            }
+            try
+            {
+                Korisnik? e;
+                try
+                {
+                    e = _context.Korisnici.Find(sifra);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(new { poruka = ex.Message });
+                }
+                if (e == null)
+                {
+                    return NotFound("Korisnik ne postoji u bazi");
+                }
+                _context.Korisnici.Remove(e);
+                _context.SaveChanges();
+                return Ok(new { poruka = "Uspješno obrisano" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { poruka = ex.Message });
+            }
 
 
         }
